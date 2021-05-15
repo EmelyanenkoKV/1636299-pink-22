d5eacca8050330f05453963addfac238cfb663d4
EmelyanenkoKV <emelyanenkoKV@gmail.com>
2021-05-15 13:57:34 +0300

    Допуск (исправление ошибок)

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,8 @@ const navMain = document.querySelector(".main-nav");
 const navToggle = document.querySelector(".main-nav__toggle");
 
 navMain.classList.remove("main-nav--nojs");
+navMain.classList.remove("main-nav--opened");
+navMain.classList.add("main-nav--closed");
 
 navToggle.addEventListener("click", function () {
   if (navMain.classList.contains("main-nav--closed")) {
@@ -13,31 +15,31 @@ navToggle.addEventListener("click", function () {
   }
 });
 
-const formButton = document.querySelector(".form-feedback__button");
+const formButton = document.querySelector(".form-user__button");
 const modalSuccess = document.querySelector(".modal-form--success");
 const modalFailure = document.querySelector(".modal-form--failure");
 const buttonSuccess = document.querySelector(".modal-form__button--success");
 const buttonFailure = document.querySelector(".modal-form__button--failure");
-const formFeedback = document.querySelector(".form-feedback");
-const email = formFeedback.querySelector("[name = email]");
-const lastname = formFeedback.querySelector("[name = lastname]");
-const firstname = formFeedback.querySelector("[name = firstname]");
+const formUser = document.querySelector(".form-user");
+const email = formUser.querySelector("[name = email]");
+const lastname = formUser.querySelector("[name = lastname]");
+const firstname = formUser.querySelector("[name = firstname]");
 
 
-formButton.addEventListener("click", function(evt) {
+formButton.addEventListener("click", function (evt) {
   if (!lastname.value || !firstname.value || !email.value) {
     evt.preventDefault();
-    modalFailure.classList.add("modal-form--failure-show")}
-  else {
+    modalFailure.classList.add("modal-form--failure-show")
+  } else {
     evt.preventDefault();
     modalSuccess.classList.add("modal-form--success-show");
   }
 });
-buttonFailure.addEventListener("click", function(evt) {
+buttonFailure.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalFailure.classList.remove("modal-form--failure-show");
 });
-buttonSuccess.addEventListener("click", function(evt) {
+buttonSuccess.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalSuccess.classList.remove("modal-form--success-show");
 });
